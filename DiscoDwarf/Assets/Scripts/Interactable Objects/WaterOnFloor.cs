2ac19d6503ac4c2edeaba092a898b73d15fca9e6
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-12-12 15:30:38 +0100

    Water now takes happiness

--- DiscoDwarf/Assets/Scripts/Interactable Objects/WaterOnFloor.cs
+++ DiscoDwarf/Assets/Scripts/Interactable Objects/WaterOnFloor.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class WaterOnFloor : MonoBehaviour
 {
+    private HUDManager hudManager;
+    [SerializeField]
+    private float substractValue = 0.3f;
+    private void Awake()
+    {
+        hudManager = FindObjectOfType<HUDManager>();
+    }
+    private void Update()
+    {
+        hudManager.SubstractFromHappyMeter(substractValue * Time.deltaTime);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponentInChildren<ItemSlot>() && other.GetComponentInChildren<ItemSlot>().Item != null && other.GetComponentInChildren<ItemSlot>().Item.GetComponent<Broom>())

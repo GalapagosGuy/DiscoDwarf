128ab0a7cab74164af6e68b6e49e35dfe2cece5c
Zawisz <zawisza_mateusz@wp.pl>
2020-12-13 00:44:26 +0100

    Bouncing glows

---
+++ DiscoDwarf/Assets/Scripts/GlowBump.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBump : IMusicListener
+{
+    public MaterialsSettings[] materialsToBump;
+
+    [SerializeField]
+    private float decreasingSpeed = 0.5f;
+
+    private void Start()
+    {
+        for (int i = 0; i < materialsToBump.Length; i++)
+        {
+            materialsToBump[i].currentIntensity = materialsToBump[i].intensity;
+        }
+    }
+
+    public override void OnBeatStart()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void OnBeatCenter()
+    {
+        for (int i = 0; i < materialsToBump.Length; i++)
+        {
+            materialsToBump[i].currentIntensity = materialsToBump[i].intensity;
+        }
+    }
+
+    public override void OnBeatFinished()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < materialsToBump.Length; i++)
+        {
+            materialsToBump[i].currentIntensity -= Time.deltaTime * decreasingSpeed;
+
+            materialsToBump[i].material.SetVector("_EmissionColor", materialsToBump[i].originalColor * materialsToBump[i].currentIntensity);
+        }
+    }
+}
+
+
+[System.Serializable]
+public struct MaterialsSettings
+{
+    public Color originalColor;
+    public Material material;
+    public float intensity;
+    public float currentIntensity;
+}
+

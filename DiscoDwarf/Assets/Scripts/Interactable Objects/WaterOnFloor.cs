602b9f563fb164ca600890914c25c47d2cc3f8ac
Zawisz <zawisza_mateusz@wp.pl>
2020-12-13 03:30:20 +0100

    Improved scene and particles for water

--- DiscoDwarf/Assets/Scripts/Interactable Objects/WaterOnFloor.cs
+++ DiscoDwarf/Assets/Scripts/Interactable Objects/WaterOnFloor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WaterOnFloor : MonoBehaviour
 {
+    public GameObject particleEffect = null;
+
     private HUDManager hudManager;
     [SerializeField]
     private float substractValue = 0.2f;
@@ -14,16 +16,25 @@ public class WaterOnFloor : MonoBehaviour
     {
         hudManager = FindObjectOfType<HUDManager>();
     }
+
     private void Update()
     {
         hudManager.SubstractFromHappyMeter(substractValue * Time.deltaTime);
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponentInChildren<ItemSlot>() && other.GetComponentInChildren<ItemSlot>().Item != null && other.GetComponentInChildren<ItemSlot>().Item.GetComponent<Broom>())
         {
             other.GetComponentInChildren<ItemSlot>().Item.GetComponent<Broom>().PlayBroomingSound();
             hudManager.AddPoints(pointBonus);
+
+            if (particleEffect)
+            {
+                GameObject ps = Instantiate(particleEffect, this.transform.position, Quaternion.identity);
+                Destroy(ps, 3.0f);
+            }
+
             Destroy(this.gameObject, 0.1f);
         }
     }
